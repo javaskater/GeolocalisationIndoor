04a1c54e5fdbe52346546dfbcbf2a20460a6a55b
Jean-Pierre MENA <jpm@jpmena.eu>
2020-04-07 10:34:13 +0200

    création du projet sous Android Studio

---
+++ app/src/main/java/fr/cnam/nfa024/jpmena/geolocalisationindoor/MainActivity.java
@@ -0,0 +1,14 @@
+
+package fr.cnam.nfa024.jpmena.geolocalisationindoor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
